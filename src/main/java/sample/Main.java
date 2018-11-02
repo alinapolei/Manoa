d100c1450b226f94dd383ed16bcd1ac3b9cbe75c
Dror <droruri3@gmail.com>
2018-11-02 14:37:24 +0200

    Using Jsoup Packge to separte docs

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,24 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.HashSet;
-import java.util.List;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         HashSet<Element> docs = new HashSet<>();
-
         ReadFile rf = new ReadFile("C:\\Users\\Dror\\Desktop\\corpus",docs);
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
