d0fa67c4753df9fb35d0db422549263eb58e2183
alina <alinapolei96@gmail.com>
2018-11-03 16:04:17 +0200

    stemmer added

--- src/main/java/sample/Parse.java
+++ src/main/java/sample/Parse.java
@@ -1,59 +1,83 @@
 package sample;
+import org.jsoup.helper.StringUtil;
+
 import java.text.DateFormatSymbols;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+
 public class Parse {
-    public Parse(HashSet<String> docs) {
-        //docs is the documents after readFile separate them all
+    public Parse(HashSet<String> docs) { //docs is the documents after readFile separate them all
         DateFormatSymbols dateFormatSymbols = new DateFormatSymbols();
         List<String> months = Arrays.asList(dateFormatSymbols.getMonths());
         months.replaceAll(String::toLowerCase);
         List<String> shortMonths = Arrays.asList(dateFormatSymbols.getShortMonths());
         shortMonths.replaceAll(String::toLowerCase);
 
-        for (String doc : docs){
+        Stemmer stemmer = new Stemmer();
+        HashSet<String> terms = new HashSet<>();
 
+        for (String doc : docs){
             String[] tokens = doc.split(" ");
             for(int i=0; i<tokens.length; i++){
                 String tok = tokens[i];
                 boolean isDollar = false;
 
+                //clean the token -> remove , . | \n from the start and the end
+                if(tok.equals("")) continue;
+                if(tok.startsWith(".") || tok.startsWith(",") || tok.startsWith("|") || tok.startsWith("\n") || tok.startsWith("(") || tok.startsWith(")"))
+                    tok = tok.substring(1, tok.length());
+                if(tok.endsWith(".") || tok.endsWith(",") || tok.endsWith("|") || tok.endsWith("\n")|| tok.endsWith("(") || tok.endsWith(")"))
+                    tok = tok.substring(0, tok.length()-1);
+
+                //parsing for words
+                if(tok.matches("[A-Za-z]+")) {
+                    tok = stemmer.stem(tok);
+                    //------put here stop words and parse for words
+                }
+
+                //parsing for numbers, prises, percent
                 if(!tok.equals("") && (tok.charAt(0) == '$' || tok.charAt(tok.length()-1) == '$')) {
                     tok = tok.replace("$", "");
                     isDollar = true;
                 }
 
-                if(tok.matches("[0-9]+\\.?[0-9]?+") && i+1<tokens.length){
+                if(tok.matches("[0-9]+\\.?[0-9]?+") && i+1<tokens.length && !tokens[i+1].matches("[0-9]+\\/[0-9]+")){
                     //number + Thousand/Million/Billion/Trillion
-                    if(tokens[i+1].matches("[0-9]+\\/[0-9]+"))
-                        continue;
-                    else if(tokens[i+1].toLowerCase().matches("thousand"))
+                    if(tokens[i+1].toLowerCase().matches("thousand")) {
                         tok = tok + "K";
-                    else if(tokens[i+1].toLowerCase().matches("million"))
+                        tokens[i + 1] = "";
+                    }
+                    else if(tokens[i+1].toLowerCase().matches("million")) {
                         tok = tok + "M";
-                    else if(tokens[i+1].toLowerCase().matches("billion"))
+                        tokens[i + 1] = "";
+                    }
+                    else if(tokens[i+1].toLowerCase().matches("billion")) {
                         tok = tok + "B";
-                    else if(tokens[i+1].toLowerCase().matches("trillion"))
-                        tok = tok +"000B";
-                    else if(tokens[i+1].matches("percent") || tokens[i+1].matches("percentage"))
+                        tokens[i + 1] = "";
+                    }
+                    else if(tokens[i+1].toLowerCase().matches("trillion")) {
+                        tok = tok + "000B";
+                        tokens[i + 1] = "";
+                    }
+                    else if(tokens[i+1].matches("percent") || tokens[i+1].matches("percentage")) {
                         tok = tok + "%";
-                    else if(months.contains(tokens[i+1].toLowerCase()) || shortMonths.contains(tokens[i+1].toLowerCase())) {
+                        tokens[i + 1] = "";
+                    }
+                    else if(!tokens[i+1].equals("") && (months.contains(tokens[i+1].toLowerCase()) || shortMonths.contains(tokens[i+1].toLowerCase()))) {
+                        //14 May -> 05-14
                         try {
                             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd");
-                            Date date = new Date();
-                            date.setMonth(new SimpleDateFormat("MMMM").parse(tokens[i+1]).getMonth());
-                            date.setDate(Integer.parseInt(tok));
-                            tok = simpleDateFormat.format(date);
+                            Calendar date = new GregorianCalendar();
+                            date.set(Calendar.MONTH, new SimpleDateFormat("MMMM").parse(tokens[i+1]).getMonth());
+                            date.set(Calendar.DAY_OF_MONTH, Integer.parseInt(tok));
+                            tok = simpleDateFormat.format(date.getTime());
+                            tokens[i+1] = "";
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
                     }
-
-                    else
-                        continue;
-                    tokens[i+1] = "";
                 }
 
                 if(tok.matches("\\d{1,3}\\,\\d\\d\\d")) {
@@ -88,15 +112,15 @@ public class Parse {
                         tok = parts[0].substring(0, parts[0].length()-9) + "." + parts[0].substring(parts[0].length()-9) + parts[1] + "B";
                 }
 
-                if(!tok.equals("") && i+1<tokens.length && !tokens[i+1].equals("") && (months.contains(tok.toLowerCase()) || shortMonths.contains(tok.toLowerCase()))){
+                if(!tok.equals("") && i+1<tokens.length && !tokens[i+1].equals("") && StringUtil.isNumeric(tokens[i+1])
+                        && (months.contains(tok.toLowerCase()) || shortMonths.contains(tok.toLowerCase()))){
                     SimpleDateFormat simpleDateFormat;
                     Calendar date = new GregorianCalendar();
-
-                    if(tokens[i+1].length() == 4){//the date is with year
+                    if(tokens[i+1].length() == 4){//May 1994 -> 1994-05
                         date.set(Calendar.YEAR, Integer.parseInt(tokens[i+1]));
                         simpleDateFormat = new SimpleDateFormat("yyyy-MM");
                     }
-                    else {//the date is with month
+                    else {//JUNE 4 -> 06-04
                         simpleDateFormat = new SimpleDateFormat("MM-dd");
                         date.set(Calendar.DAY_OF_MONTH, Integer.parseInt(tokens[i+1]));
                     }
@@ -109,19 +133,26 @@ public class Parse {
                     }
                 }
 
-                if(!tok.equals("") && tok.charAt(tok.length()-1) == 'm')
+                if(!tok.equals("") && tok.charAt(tok.length()-1) == 'm' && tok.substring(0, tok.length()-1).matches("\\d+\\.?\\d+?"))
                     tok = tok.replace('m', 'M');
-                else if(tok.length()>=2 && tok.charAt(tok.length()-2) == 'b' && tok.charAt(tok.length()-1) == 'n')
+                else if(tok.length()>=2 && tok.charAt(tok.length()-2) == 'b' && tok.charAt(tok.length()-1) == 'n' && tok.substring(0, tok.length()-2).matches("\\d+\\.?\\d+?"))
                     tok = tok.replace("bn", "B");
 
-                    if(tok.equals("U.S.") && !tokens[i+1].equals("") && tokens[i+1].equals("dollars")) {
+                if(tok.equals("U.S.") && i+1<tokens.length && tokens[i+1].equals("dollars")) {
                     tok = "Dollars";
                     tokens[i+1] = "";
                 }
 
+                if(tok.equals("between") && i+3<tokens.length && tokens[i+2].equals("and")) {
+                    tok = tok + tokens[i + 1] + tokens[i + 2] + tokens[i + 3];
+                    tokens[i+1] = ""; tokens[i+2] = ""; tokens[i+3] = "";
+                }
+
                 if(isDollar)
                     tok = tok + " Dollars";
+
                 tokens[i] = tok;
+                terms.add(tok);
             }
 
             doc = String.join(" ", tokens);
