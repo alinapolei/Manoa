c7c991547e2a69a1e6cf35947f8f13fd1558f336
Dror <droruri3@gmail.com>
2018-12-24 16:55:45 +0200

    PartB 0.2
    Signed-off-by: Dror <droruri3@gmail.com>

--- src/main/java/sample/Searcher.java
+++ src/main/java/sample/Searcher.java
@@ -1,19 +1,26 @@
 package sample;
 
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+
 import java.util.*;
 
 public class Searcher {
     HashMap<Queryy, HashSet<String>> finalTokens = new HashMap<>();
     Parse parse;
 
-    public HashMap<Queryy, HashSet<String>> search(Queue<Queryy> queryys,boolean isStem) {
+    public HashMap<Queryy, HashSet<String>> search(Queue<Queryy> queryys,boolean isStem) throws UnirestException {
         parse=new Parse();
         while(!queryys.isEmpty()) {
             Queryy query=queryys.poll();
             parse.doQuereyparse(query, isStem, finalTokens);
         }
         removeStopwords();
+        getSemanitcWord(finalTokens);
         return finalTokens;
     }
 
@@ -27,4 +34,26 @@ public class Searcher {
         }
     }
 
+    private void getSemanitcWord( HashMap<Queryy, HashSet<String>> words) throws UnirestException {
+        List<String> tmp=new ArrayList<>();
+        for (HashSet<String> x:words.values()) {
+            for(String y : x) {
+                HttpResponse<JsonNode> response = Unirest.get("https://api.datamuse.com/words?ml=" + y)
+                        .header("X-Mashape-Key", "<required>")
+                        .header("Accept", "application/json")
+                        .asJson();
+                Object map = (response.getBody().getArray());
+                if(map!=null)
+                    for (int i = 0; i<5; i++)
+                    {
+                        String addWord=((JSONArray) map).getJSONObject(i).get("word").toString();
+                        tmp.add(addWord);
+                    }
+            }
+            for (String i:tmp)
+                x.add(i);
+            tmp.clear();
+        }
+
+    }
 }
